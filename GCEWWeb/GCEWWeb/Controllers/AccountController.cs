75835599eef7612f66fdc83f3f7d44059762c54e
Maxim Zimin <zim.ma2010@yandex.ru>
2019-03-14 06:04:01 +0300

    Init Account Controller and Models

--- GCEWWeb/GCEWWeb/Controllers/AccountController.cs
+++ GCEWWeb/GCEWWeb/Controllers/AccountController.cs
@@ -2,13 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GCEWWeb.Models.ControllerModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GCEWWeb.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel registerModel)
         {
             return View();
         }
---
+++ GCEWWeb/GCEWWeb/Models/ControllerModels/RegisterModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GCEWWeb.Models.ControllerModels
+{
+    public enum Plan
+    {
+        Free,
+        /* Какие-то еще планы */
+    }
+
+
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Login is required")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "This is not email")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Password is requried")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Min Length is 6")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Password is requried")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Min Length is 6")]
+        [Compare("Password", ErrorMessage = "Password != Confirmed Password")]
+        public string ConfirmedPassword { get; set; }
+        [Required(ErrorMessage = "Plan is required")]
+        public Plan Plan { get; set; } = Plan.Free;
+    }
+}
