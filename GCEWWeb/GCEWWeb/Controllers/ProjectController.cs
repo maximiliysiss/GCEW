0c5823dcd2d251b9abd4fb70b3371aaba10e7ee1
Maxim Zimin <zim.ma2010@yandex.ru>
2019-04-22 11:46:00 +0600

    Add modal windows

---
+++ GCEWWeb/GCEWWeb/Controllers/ProjectController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using GCEWWeb.Models;
+using GCEWWeb.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GCEWWeb.Controllers
+{
+    public class ProjectController : BaseController
+    {
+        public ProjectController(DatabaseContext databaseContext, CustomConfiguration options) : base(databaseContext, options)
+        {
+        }
+
+        [HttpPost]
+        public bool CreateFile(string name, int projectID)
+        {
+            var project = DatabaseContext.Projects.Find(projectID);
+            if (project == null)
+                return false;
+
+            System.IO.File.Create(Path.Combine(project.Path, name, ".graphic")).Close();
+            return true;
+        }
+
+        [HttpPost]
+        public bool CopyFile(string name)
+        {
+            var fileName = Path.GetFileName(name).Split(".")[0];
+            System.IO.File.Copy(name, Path.Combine(Path.GetDirectoryName(name), fileName, "New.graphic"));
+            return true;
+        }
+
+        [HttpPost]
+        public bool DeleteFile(string name)
+        {
+            System.IO.File.Delete(name);
+            return true;
+        }
+    }
+}
---
+++ GCEWWeb/GCEWWeb/wwwroot/js/custom-modal-window.js
@@ -0,0 +1,22 @@
+﻿
+
+$(".custom-modal-holder").click(function () {
+    $("#" + $(this).attr("modal")).css("display", "block");
+});
+
+$(".close-modal").click(function () {
+    $("#" + $(this).attr("modal")).css("display", "none");
+});
+
+$(document).click(function () {
+    $("custom-modal").css("display", "none");
+});
+
+$(".modal-content").click(function () {
+    event.preventDefault();
+    event.stopPropagation();
+});
+
+$(".custom-modal").click(function () {
+    $(this).css("display", "none");
+});
