5dd7a3cec494eec839b5a4a36f1336158ec2171d
Maxim Zimin <zim.ma2010@yandex.ru>
2019-05-29 10:52:34 +0300

    Add IsTask field

--- GCEWWeb/GCEWWeb/Models/Database/Project.cs
+++ GCEWWeb/GCEWWeb/Models/Database/Project.cs
@@ -2,6 +2,7 @@
 using GCEWWeb.Models.Database;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,7 +16,9 @@ namespace GCEWWeb.Models.Database
         public string Path { get; set; }
         public double Size { get; set; }
         public DateTime DateTime { get; set; }
+        public bool IsTask { get; set; }
         public User User { get; set; }
+        [NotMapped]
         public string FileSystemTree
         {
             get
@@ -25,7 +28,7 @@ namespace GCEWWeb.Models.Database
                 return new DirectoryInfo(Path).ToStringTree();
             }
         }
-
+        [NotMapped]
         public string FileSystemTreeJSON
         {
             get
