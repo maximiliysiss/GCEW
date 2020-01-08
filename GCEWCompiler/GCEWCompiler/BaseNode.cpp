f0a80cba0c72a6c00c1944985c6aef8df8edd110
Maxim Zimin <zim.ma2010@yandex.ru>
2020-01-08 05:03:39 +0300

    compile "fuck end"

--- GCEWCompiler/GCEWCompiler/BaseNode.cpp
+++ GCEWCompiler/GCEWCompiler/BaseNode.cpp
@@ -1,10 +1,22 @@
 #include "BaseNode.h"
+#include "Term.h"
 
 namespace gcew::trees::parser
 {
+	std::string BaseNode::tryGetType()
+	{
+		if (dynamic_cast<Term*>(this)) {
+			return dynamic_cast<Term*>(this)->getType();
+		}
+		else if (dynamic_cast<Node*>(this)) {
+			return dynamic_cast<Node*>(this)->tryGetType();
+		}
+		return std::string();
+	}
 
 	BaseNode::BaseNode()
 	{
+		name = gcew::commons::createUniqueGUID();
 	}
 
 	BaseNode::~BaseNode()
